903ff370286f3dc462bd857bb763e21b2a79cf66
David Cheng <dkcheng10@gmail.com>
2017-12-21 20:00:22 -0800

    Added basic blueprint of what we need in the JS file

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+//Components that we need in general terms
+// What should appear when the page opens. Dynamically generated
+// Install firebase
+//Ajax call for PetFinder
+//Ajax call for Google Places
+// Match results to Google maps
