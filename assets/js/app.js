ede02f65e041405bef2ebaf8c5c0022a2a7e75d7
AhmetK <ahmetburhan@gmail.com>
2017-12-21 21:06:32 -0800

    firbase added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDWOzsO-5lWbobwqB0XodtPUEbvHaei6pU",
+    authDomain: "petproject-ebbc1.firebaseapp.com",
+    databaseURL: "https://petproject-ebbc1.firebaseio.com",
+    projectId: "petproject-ebbc1",
+    storageBucket: "",
+    messagingSenderId: "1068542777472"
+  };
+  firebase.initializeApp(config);
+
+  
+
 //Components that we need in general terms
 // What should appear when the page opens. Dynamically generated
 // Install firebase
