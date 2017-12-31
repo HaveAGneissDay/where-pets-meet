ca9469cd704011c128a09ea6b290a6fdc55508c0
enochwu <enochwu7@gmail.com>
2017-12-31 15:01:14 -0800

    HTML to JS to API
    Data from inputs is now being fetched from DOM and utilized in JS. Selecting an animal from the second dropdown and a zipcode will now log both those items and a dynamically generated query URL to be utilized in the AJAX call. Returned information from user input is now logged in console (25 results).

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
   // Required init for dropdowns to work.
   $(".rotate").rotator();
 
+  // -----------------------------------------------------------
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDWOzsO-5lWbobwqB0XodtPUEbvHaei6pU",
@@ -20,82 +22,93 @@ $(document).ready(function() {
 
   var database = firebase.database();
 
-  var soughtAnimal = "dog" // $(soughtAnimal).text;
-  console.log(soughtAnimal);
-  var queryURL = 'https://api.petfinder.com/pet.find?format=json&key=dd9016ebaee01ff97c4bd3319ee97eaf&animal=' + soughtAnimal + '&location=94025&?count=5&callback=?';
+  $("#submit").on("click", function() {
 
-  $.ajax({
-             type: 'GET',
-             url: queryURL,
-             processData: true,
-             data: {},
-             dataType: "json",
-             success: function (petApiData) {
-                 processData(petApiData);
-             }
+    // -----------------------------------------------------------
+    var zipInput = $('#zip_code').val().trim()
+    console.log(zipInput);
+
+    var soughtAnimal = $('#choiceAnimal>option:selected').val().trim();
+    console.log(soughtAnimal);
+
+    var queryURL = 'https://api.petfinder.com/pet.find?format=json&key=dd9016ebaee01ff97c4bd3319ee97eaf&animal=' + soughtAnimal + '&location=94025&?count=5&callback=?';
+    console.log(queryURL);
+
+    $.ajax({
+               type: 'GET',
+               url: queryURL,
+               processData: true,
+               data: {},
+               dataType: "json",
+               success: function (petApiData) {
+                   processData(petApiData);
+               }
+    });
+
+    function processData(petApiData){
+     //Do some stuff with the data
+     var results = petApiData.petfinder.pets
+
+     for (var i = 0; i < 25; i++) {
+
+       // var breed1 = results.pet[i].breeds.breed[0].$t
+       // var breed2 = results.pet[i].breeds.breed[1].$t
+       var petName = results.pet[i].name.$t
+       console.log(petName);
+
+       var petGender = results.pet[i].sex.$t
+       console.log(petGender);
+
+       var petPhone = results.pet[i].contact.phone.$t
+       console.log(petPhone);
+
+       var petEmail = results.pet[i].contact.email.$t
+       console.log(petEmail);
+
+       var aboutPet = results.pet[i].description.$t
+       console.log(aboutPet);
+
+       var petCity = results.pet[i].contact.city.$t
+       console.log(petCity);
+
+       var petState = results.pet[i].contact.state.$t
+       console.log(petState);
+
+       var petZipcode = results.pet[i].contact.zip.$t
+       console.log(petZipcode);
+
+       console.log("--------");
+       console.log(petCity+" "+petState+" "+petZipcode);
+
+       console.log('Image source link:');
+       console.log(results.pet[i].media.photos.photo[2].$t);
+
+       // console.log(breed1);
+       // console.log(breed2);
+       console.log("____________________");
+
+
+       // // Creating and storing a div tag
+       // var animalPic = $("<img />");
+       //
+       // // Setting the src attribute of the image to a property pulled off the result item
+       // animalPic.addClass('animalPic img-rounded media-middle');
+       // animalPic.attr("src", results[i].images.fixed_height.url);
+       // animalPic.attr("data-animate", results[i].images.fixed_height.url);
+       // animalPic.attr("data-still", results[i].images.fixed_height_still.url);
+       // animalPic.attr("data-still", results[i].images.fixed_height_still.url);
+       // animalPic.attr("data-state", "animate");
+       //
+       // // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+       // $("#gifSpace").prepend(animalPic);
+
+     }
+
+    }
+
+    // -----------------------------------------------------------
   });
-
-  function processData(petApiData){
-   //Do some stuff with the data
-   var results = petApiData.petfinder.pets
-
-   for (var i = 0; i < 25; i++) {
-
-     // var breed1 = results.pet[i].breeds.breed[0].$t
-     // var breed2 = results.pet[i].breeds.breed[1].$t
-     var petName = results.pet[i].name.$t
-     console.log(petName);
-
-     var petGender = results.pet[i].sex.$t
-     console.log(petGender);
-
-     var petPhone = results.pet[i].contact.phone.$t
-     console.log(petPhone);
-
-     var petEmail = results.pet[i].contact.email.$t
-     console.log(petEmail);
-
-     var aboutPet = results.pet[i].description.$t
-     console.log(aboutPet);
-
-     var petCity = results.pet[i].contact.city.$t
-     console.log(petCity);
-
-     var petState = results.pet[i].contact.state.$t
-     console.log(petState);
-
-     var petZipcode = results.pet[i].contact.zip.$t
-     console.log(petZipcode);
-
-     console.log("--------");
-     console.log(petCity+" "+petState+" "+petZipcode);
-
-     console.log('Image source link:');
-     console.log(results.pet[i].media.photos.photo[2].$t);
-
-     // console.log(breed1);
-     // console.log(breed2);
-     console.log("____________________");
-
-
-     // // Creating and storing a div tag
-     // var animalPic = $("<img />");
-     //
-     // // Setting the src attribute of the image to a property pulled off the result item
-     // animalPic.addClass('animalPic img-rounded media-middle');
-     // animalPic.attr("src", results[i].images.fixed_height.url);
-     // animalPic.attr("data-animate", results[i].images.fixed_height.url);
-     // animalPic.attr("data-still", results[i].images.fixed_height_still.url);
-     // animalPic.attr("data-still", results[i].images.fixed_height_still.url);
-     // animalPic.attr("data-state", "animate");
-     //
-     // // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-     // $("#gifSpace").prepend(animalPic);
-
-   }
-
-  }
-
+  
 });
 
 //Components that we need in general terms
