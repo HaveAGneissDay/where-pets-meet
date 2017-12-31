a09ea92306cd1fb6f150cefcb6ae80ad5f5ffa2e
enochwu <enochwu7@gmail.com>
2017-12-31 11:33:46 -0800

    Addition JS cleaning.
    Recognized that doc.ready was closing way too early and not including code below the close. More indenting done to clear up code.

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,83 +20,83 @@ $(document).ready(function() {
 
   var database = firebase.database();
 
+  var soughtAnimal = "dog" // $(soughtAnimal).text;
+  console.log(soughtAnimal);
+  var queryURL = 'https://api.petfinder.com/pet.find?format=json&key=dd9016ebaee01ff97c4bd3319ee97eaf&animal=' + soughtAnimal + '&location=94025&?count=5&callback=?';
+
+  $.ajax({
+             type: 'GET',
+             url: queryURL,
+             processData: true,
+             data: {},
+             dataType: "json",
+             success: function (petApiData) {
+                 processData(petApiData);
+             }
+  });
+
+  function processData(petApiData){
+   //Do some stuff with the data
+   var results = petApiData.petfinder.pets
+
+   for (var i = 0; i < 25; i++) {
+
+     // var breed1 = results.pet[i].breeds.breed[0].$t
+     // var breed2 = results.pet[i].breeds.breed[1].$t
+     var petName = results.pet[i].name.$t
+     console.log(petName);
+
+     var petGender = results.pet[i].sex.$t
+     console.log(petGender);
+
+     var petPhone = results.pet[i].contact.phone.$t
+     console.log(petPhone);
+
+     var petEmail = results.pet[i].contact.email.$t
+     console.log(petEmail);
+
+     var aboutPet = results.pet[i].description.$t
+     console.log(aboutPet);
+
+     var petCity = results.pet[i].contact.city.$t
+     console.log(petCity);
+
+     var petState = results.pet[i].contact.state.$t
+     console.log(petState);
+
+     var petZipcode = results.pet[i].contact.zip.$t
+     console.log(petZipcode);
+
+     console.log("--------");
+     console.log(petCity+" "+petState+" "+petZipcode);
+
+     console.log('Image source link:');
+     console.log(results.pet[i].media.photos.photo[2].$t);
+
+     // console.log(breed1);
+     // console.log(breed2);
+     console.log("____________________");
+
+
+     // // Creating and storing a div tag
+     // var animalPic = $("<img />");
+     //
+     // // Setting the src attribute of the image to a property pulled off the result item
+     // animalPic.addClass('animalPic img-rounded media-middle');
+     // animalPic.attr("src", results[i].images.fixed_height.url);
+     // animalPic.attr("data-animate", results[i].images.fixed_height.url);
+     // animalPic.attr("data-still", results[i].images.fixed_height_still.url);
+     // animalPic.attr("data-still", results[i].images.fixed_height_still.url);
+     // animalPic.attr("data-state", "animate");
+     //
+     // // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+     // $("#gifSpace").prepend(animalPic);
+
+   }
+
+  }
+
 });
-
-var soughtAnimal = "dog" // $(soughtAnimal).text;
-console.log(soughtAnimal);
-var queryURL = 'https://api.petfinder.com/pet.find?format=json&key=dd9016ebaee01ff97c4bd3319ee97eaf&animal=' + soughtAnimal + '&location=94025&?count=5&callback=?';
-
-$.ajax({
-           type: 'GET',
-           url: queryURL,
-           processData: true,
-           data: {},
-           dataType: "json",
-           success: function (petApiData) {
-               processData(petApiData);
-           }
-});
-
-function processData(petApiData){
- //Do some stuff with the data
- var results = petApiData.petfinder.pets
-
- for (var i = 0; i < 25; i++) {
-
-   // var breed1 = results.pet[i].breeds.breed[0].$t
-   // var breed2 = results.pet[i].breeds.breed[1].$t
-   var petName = results.pet[i].name.$t
-   console.log(petName);
-
-   var petGender = results.pet[i].sex.$t
-   console.log(petGender);
-
-   var petPhone = results.pet[i].contact.phone.$t
-   console.log(petPhone);
-
-   var petEmail = results.pet[i].contact.email.$t
-   console.log(petEmail);
-
-   var aboutPet = results.pet[i].description.$t
-   console.log(aboutPet);
-
-   var petCity = results.pet[i].contact.city.$t
-   console.log(petCity);
-
-   var petState = results.pet[i].contact.state.$t
-   console.log(petState);
-
-   var petZipcode = results.pet[i].contact.zip.$t
-   console.log(petZipcode);
-
-   console.log("--------");
-   console.log(petCity+" "+petState+" "+petZipcode);
-
-   console.log('Image source link:');
-   console.log(results.pet[i].media.photos.photo[2].$t);
-
-   // console.log(breed1);
-   // console.log(breed2);
-   console.log("____________________");
-
-
-   // // Creating and storing a div tag
-   // var animalPic = $("<img />");
-   //
-   // // Setting the src attribute of the image to a property pulled off the result item
-   // animalPic.addClass('animalPic img-rounded media-middle');
-   // animalPic.attr("src", results[i].images.fixed_height.url);
-   // animalPic.attr("data-animate", results[i].images.fixed_height.url);
-   // animalPic.attr("data-still", results[i].images.fixed_height_still.url);
-   // animalPic.attr("data-still", results[i].images.fixed_height_still.url);
-   // animalPic.attr("data-state", "animate");
-   //
-   // // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-   // $("#gifSpace").prepend(animalPic);
-
- }
-
-}
 
 //Components that we need in general terms
 // What should appear when the page opens. Dynamically generated
